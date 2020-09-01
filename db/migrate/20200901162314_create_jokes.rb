f20058ca09543f0e58ece07639d1a304a6c1ef7a
Terrell Cooper <TerrellCoop@gmail.com>
2020-09-01 10:29:37 -0600

    boiler backend set up

---
+++ db/migrate/20200901162314_create_jokes.rb
@@ -0,0 +1,11 @@
+class CreateJokes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :jokes do |t|
+      t.string :type
+      t.text :setup
+      t.text :punchline
+
+      t.timestamps
+    end
+  end
+end
