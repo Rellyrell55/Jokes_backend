2fc17ecdfdaafc7c480203711c3e35f68c20606d
estein1988 <estein1988@gmail.com>
2020-09-01 11:23:55 -0600

    Attempt seed data with API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'pry'
+require 'rest-client'
+require 'json'
+
+base_url = "https://official-joke-api.appspot.com/jokes/random"
+data = RestClient.get(base_url)
+parsed_data = JSON.parse(data)
+binding.pry
 
 
-
+parsed_data.map do |joke|
+    Joke.create(type: joke["type"], setup: joke["setup"], punchline: joke["punchline"])
+end
